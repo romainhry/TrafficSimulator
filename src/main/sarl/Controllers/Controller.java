23b72d0c42da3a7a1708bf44469f6584ed92a847
Romain Henry <romainhry@gmail.com>
2017-06-13 09:38:59 +0200

    delete imports

--- src/main/sarl/Controllers/Controller.java
+++ src/main/sarl/Controllers/Controller.java
@@ -6,8 +6,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseDragEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
@@ -15,7 +13,6 @@ import main.Main;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.ResourceBundle;
 import java.util.UUID;
 
