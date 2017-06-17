f6e004836f1fcffbc79363e9ebc14c19030788ca
Geroo <gero_mohe2@hotmail.com>
2017-06-17 14:53:45 +0200

    player

---
+++ src/main/sarl/Controllers/KeyBoardInputs.java
@@ -0,0 +1,49 @@
+package Controllers;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import model.Car.CarObject;
+import model.math.Point2f;
+/*This class will let us to manage the Keyboards inputs and then determine
+ * the corresponding action that the player car will execute*/
+public class KeyBoardInputs implements EventHandler<KeyEvent>{
+	CarObject playerCar;
+	public void KeyboardInputs(){
+		playerCar = new CarObject(0,0);
+	}
+    @Override
+    public void handle(KeyEvent e) {
+    	try{
+    	if (e.getCode() == KeyCode.UP) {
+    		this.playerCar.setInfluence(new Point2f(3,0));
+			//System.out.println("up");
+		}else if (e.getCode() == KeyCode.DOWN) {
+			this.playerCar.setInfluence(new Point2f(-3,0));
+			//System.out.println("down");
+		}else if (e.getCode() == KeyCode.LEFT) {
+			if (playerCar.getDirection() > 1) {
+				playerCar.setTurnTo(1);
+			}
+			//System.out.println("left");
+		}else if (e.getCode() == KeyCode.RIGHT) {
+			if (playerCar.getDirection() > 2) {
+				playerCar.setTurnTo(2);
+			}
+			//System.out.println("right");
+		}else if (e.getCode() == KeyCode.ENTER) {
+			//claxon
+			System.out.println("claxon");
+		}
+    	}catch(Exception exc){
+    	System.out.println(exc);	
+    	}
+    }
+    public CarObject getPlayerCar(){
+    	return this.playerCar;
+    }
+    public void setPlayerCar(CarObject p){
+    	this.playerCar = p;
+    }
+}
--- src/main/sarl/main/Main.java
+++ src/main/sarl/main/Main.java
@@ -1,8 +1,18 @@
 package main;
+import java.awt.AWTEvent;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javafx.scene.input.KeyEvent;
+
+import javax.swing.JFrame;
+
+import Controllers.KeyBoardInputs;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 /**
@@ -11,23 +21,24 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
     public static Stage stage;
-    @Override
+    
+    //@Override
     public void start(Stage stage) throws Exception {
        //StageStyle.UNDECORATED
         Parent root = FXMLLoader.load(getClass().getResource("../GUI/Vue.fxml"));
         Scene scene = new Scene(root);
+        scene.addEventHandler(KeyEvent.KEY_PRESSED,new KeyBoardInputs());
         stage.setScene(scene);
         //stage.setMaxHeight(800);
         //stage.setMaxWidth(1080);
         stage.sizeToScene();
-        
         stage.setResizable(true);
         stage.show();
         Main.stage = stage;
     }
     
     public static void main(String[] args) {
-        launch(args);
+    	launch(args);
     }
 }
 
